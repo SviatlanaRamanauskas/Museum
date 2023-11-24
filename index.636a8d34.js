eeb8e193f2bd89f47c1cde65d8ac3e24a51e846b
Svitlana Ramanauskas <ramanauskassvs@gmail.com>
2023-11-24 18:52:40 +0200

    make build

---
+++ index.636a8d34.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".header"),a=document.querySelector(".header__menu--link");a.addEventListener("hashchange",function(){"#menu"===a.hash?e.classList.add("page__menu"):e.classList.remove("page__menu")});//# sourceMappingURL=index.636a8d34.js.map
+
+//# sourceMappingURL=index.636a8d34.js.map
